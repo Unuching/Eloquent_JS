08508a7658fa01158a591b542cb69e28206ee978
Unuching Marma <unuching.marma2023@gmail.com>
2025-08-01 10:03:23 +0600

    update code

---
+++ program/ctrlFlow.js
@@ -0,0 +1,5 @@
+let yourNumber = prompt("pick a number")
+let result = Math.floor(Math.sqrt(yourNumber))
+
+
+document.getElementById("result").innerHTML = result
---
+++ program/msx.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(Math.max(...numbers)); // spread operator
+
+console.log((Math.min(...numbers)));
+
